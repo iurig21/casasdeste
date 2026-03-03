81e951af6bd85c8f4771d84d264efc281bd6a8be
iurig21 <a32192@alunos.ipca.pt>
2026-03-03 14:30:15 +0000

    fix: fixed the projects font

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,8 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Casas D'Este</title>
-    <link rel="preconnect" href="<https://fonts.bunny.net>">
+    <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
@@ -36,7 +39,7 @@
 
                     <button class="site-nav__brochure">
                         <span>Faça download</span>
-                        <span>de brochura</span>
+                        <span>da brochura</span>
                     </button>
                 </div>
             </div>

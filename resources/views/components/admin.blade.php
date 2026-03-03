6e01bc01ce40e446752b72106fbccdd89f2f1c54
iurig21 <a32192@alunos.ipca.pt>
2026-03-03 14:03:18 +0000

    fix: acabamentos page ui

---
+++ resources/views/components/admin.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br" data-theme="lofi">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Casas D'Este - Backoffice</title>
+    <link rel="preconnect" href="<https://fonts.bunny.net>">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>  
+
+</body>
+</html>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,7 @@
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 

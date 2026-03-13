23c6ac062ceeee75a4cceae92446ec7b93decbba
iurig21 <a32192@alunos.ipca.pt>
2026-03-13 17:37:31 +0000

    feat: rate limiting on brochure donwload and contact routes

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,14 @@ Route::get('/acabamentos', function () {
     return view('acabamentos');
 });
 
-Route::post('/brochure-download', [BrochureDownloadController::class, 'store'])->name('brochure.download');
+Route::middleware(['throttle:api'])->group(function () {
+    Route::post('/brochure-download', [BrochureDownloadController::class, 'store'])->name('brochure.download');
+    Route::post('/contacto', [ContactController::class, 'send'])->name('contact.send');
+});
 
-Route::post('/contacto', [ContactController::class, 'send'])->name('contact.send');
 
 Route::get('/admin', [AdminController::class, 'loginForm']);
 Route::post('/admin/login', [AdminController::class, 'login']);
-
 Route::middleware('admin')->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'dashboard']);
     Route::delete('/admin/downloads/{download}', [AdminController::class, 'destroy']);

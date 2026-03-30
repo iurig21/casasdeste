dbd4e39589d1143c69c965bc946a34f4763e4b58
iurig21 <a32192@alunos.ipca.pt>
2026-03-30 10:03:58 +0100

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <section class="home-page">
         <section class="home-hero">
-            <img src="/imagens/home1.webp" alt="Casas D'Este" class="home-hero__image">
+            <img src="{{ asset('imagens/home1.webp') }}" alt="Casas D'Este" class="home-hero__image">
             <div class="home-hero__overlay" fetchpriority="high">
                 <svg width="885" height="398" viewBox="0 0 885 398" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path d="M885 397.697H-98V172.088L885 0V397.697Z" fill="#C4AA85" fill-opacity="0.85"/>
@@ -12,7 +12,7 @@
                         <p class="home-hero__subtitle">Onde a Arquitetura Moderna encontra o Refúgio Perfeito em Braga</p>
                         <p class="home-hero__description">
                             Moradias unifamiliares V3 de luxo, desenhadas para oferecer o máximo conforto, sofisticação e uma
-                            integração única com a natureza.
+                            integração única com a natureza.w
                         </p>
                     </div>
                 </div>
